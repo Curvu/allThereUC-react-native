3a119d2becd0c91ce4ac5824960601ffdbc0f493
Curvu <curvu6@gmail.com>
2023-05-05 20:09:40 +0100

    better presence handler

--- src/Views/Main.js
+++ src/Views/Main.js
@@ -1,35 +1,86 @@
-import { View, StyleSheet, Text } from 'react-native';
-import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 
 import Button from '../Components/Button';
 
-const markPresence = async (token) => {
-  console.log(token);
+const getPresence = async (token, aula) => {
+  try {
+    const response = await axios.get(`https://academic.fw.uc.pt/v1/student/class/${aula}/presence`, { headers: { 'authorization': token } });
+    return response.data; // TODO check this
+  } catch (error) {
+    return false;
+  }
+}
+
+const submitPresence = async (token, aula, session) => {
   const payload = { "type": "local" }
-  let aula, session, time;
+  try {
+    const response = await axios.post(`https://academic.fw.uc.pt/v1/student/class/${aula}/session/${session}/presence`, payload, { headers: { 'authorization': token, 'Content-Type': 'application/json' } });
+    return response.data;
+  } catch (error) {
+    return false;
+  }
+}
+
+const markPresence = async (token, setButton) => {
+  let aula, session;
 
   await axios.get('https://academic.fw.uc.pt/v1/student/sessions/next', { headers: { 'authorization': token } })
-  .then(response => {
-    // console.log(response.data);
-    aula = response.data[0].edition.key;
-    time = new Date(response.data[0].start_date);
-    session = response.data[0].key;
-  })
-  .catch(error => console.log(error));
+  .then(async response => {
+    for (let i = 0; i < response.data.length; i++) {
+      element = response.data[i];
+      // console.log(element);
+      aula = element.edition.key;
+      session = element.key;
+      let start = new Date(element.start_date);
+      let end = new Date(element.end_date);
 
-  console.log(aula, session, time);
-  // TODO: check if aula aula is ongoing
-  if (time > Date.now()) {
-    console.log('Aula ainda não começou');
-    return;
+      let now = Date.now();
+      if ((now > start && now < end)) break;
+      else {
+        aula = null;
+        session = null;
+        start = null;
+        end = null;
+      }
+    }
+  }).catch(error => {});
+  
+  console.log(aula, session);
+  if (aula && session) {
+    await getPresence(token, aula)
+    .then(presence => {
+      if (presence) {
+        setButton('e');
+        setTimeout(() => setButton(''), 1000);
+      } else {
+        submitPresence(token, aula, session)
+        .then(response => {
+          console.log(response);
+          setButton('s');
+          setTimeout(() => setButton(''), 1000);
+        }).catch(error => {
+          console.log(error);
+          setButton('e');
+          setTimeout(() => setButton(''), 1000);
+        });
+      }
+    }).catch(error => {
+      console.log(error);
+      setButton('e');
+      setTimeout(() => setButton(''), 1000);
+    });
+  } else {
+    setButton('e');
+    setTimeout(() => setButton(''), 1000);
   }
-  await axios.post(`https://academic.fw.uc.pt/v1/student/class/${aula}/session/${session}/presence`, payload, { headers: { 'authorization': token, 'Content-Type': 'application/json' } })
 }
 
 export default Main = ({token, setToken}) => {
-  console.log(token);
+  const [button, setButton] = useState('');
+
   const reset = async () => {
     AsyncStorage.clear();
     setToken(null);
@@ -38,8 +89,9 @@ export default Main = ({token, setToken}) => {
   return (
     <View style={styles.container}>
       <Button
-        onPress={() => markPresence(token)}
+        onPress={() => markPresence(token, setButton)}
         title='marcar presença'
+        state={button}
       />
       <Button
         onPress={reset}
@@ -51,7 +103,6 @@ export default Main = ({token, setToken}) => {
 
 const styles = StyleSheet.create({
   container: {
-    height: '90%',
     alignItems: 'center',
     justifyContent: 'center',
     gap: 20,
