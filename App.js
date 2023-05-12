41e4db518bb8b2557676dbd99fc6f3ea69fd15b3
Curvu <curvu6@gmail.com>
2023-05-12 16:08:07 +0100

    removing function defenition from api

--- App.js
+++ App.js
@@ -3,33 +3,17 @@ import Login from './src/Views/Login.js';
 import Main from './src/Views/Main.js'
 import { StyleSheet, KeyboardAvoidingView } from 'react-native';
 import { useEffect, useState } from 'react';
-import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
-// force white background on android
+import { tryLogin } from './src/Api.js';
 
 export default App = () => {
-  const [token, setToken] = useState(null)
-
-  const tryLogin = async (email, password) => {
-    console.log(email)
-    let payload = {
-      "email": email,
-      "password": password,
-      "longLivedToken": false,
-    }
-    return await axios.post('https://id.fw.uc.pt/v1/login', payload)
-    .then(response => response.data.token)
-    .catch(error => undefined);
-  }
+  const [token, setToken] = useState('')
 
   useEffect(() => {
-    AsyncStorage.getItem('@storage_Key').then((values) => {
+    AsyncStorage.getItem('@storage_Key').then(async (values) => {
       if (values != null) {
         let payload = JSON.parse(values);
-        tryLogin(payload.email, payload.password).then((token) => {
-          setToken(token);
-        })
+        setToken(await tryLogin(payload.email, payload.password));
       }
     })
   }, []) // Runs once it's mounted (on start)
@@ -39,7 +23,7 @@ export default App = () => {
       { token !== null ? 
         <Main token={token} setToken={setToken}/>
         :
-        <Login setToken={setToken} tryLogin={tryLogin}/>
+        <Login setToken={setToken}/>
       }
     </KeyboardAvoidingView>
   );
