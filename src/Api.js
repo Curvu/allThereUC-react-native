710c36dfd3ae5a52c306196790780b60edab43ee
Curvu <curvu6@gmail.com>
2023-05-12 16:08:20 +0100

    functions that "touch" in the API

---
+++ src/Api.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+const tryLogin = (email, password) => new Promise((resolve, reject) => {
+  axios.post('https://id.fw.uc.pt/v1/login', {email, password, longLivedToken: false})
+  .then(response => { resolve(response.data.token) }).catch(error => { resolve('') });
+});
+
+const getPresence = (token, aula) => new Promise((resolve, reject) => {
+  axios.get(`https://academic.fw.uc.pt/v1/student/class/${aula}/presence`, { headers: { 'authorization': token } })
+  .then(response => {
+    if (response.data.type == null) resolve(response.data.type);
+    else resolve(true);
+  }).catch(error => { resolve(true) });
+});
+
+const submitPresence = async (token, aula, session) => {
+  const payload = { "type": "local" }
+  try {
+    const response = await axios.post(`https://academic.fw.uc.pt/v1/student/class/${aula}/session/${session}/presence`, payload, { headers: { 'authorization': token, 'Content-Type': 'application/json' } });
+    return response.data;
+  } catch (error) {
+    return false;
+  }
+}
+
+const getAula = (token) => new Promise((resolve, reject) => {
+  axios.get('https://academic.fw.uc.pt/v1/student/sessions/next', { headers: { 'authorization': token } })
+  .then(async response => {
+    for (let i = 0; i < response.data.length; i++) {
+      element = response.data[i];
+      aula = element.edition.key;
+      session = element.key;
+      let start = new Date(element.start_date);
+      let end = new Date(element.end_date);
+
+      let now = Date.now();
+      if ((now < end)) { // now > start && now < end
+        resolve({ aula, session });
+        break;
+      } else resolve({ aula: null, session: null});
+    }
+  }).catch(error => { resolve({ aula: null, session: null}) });
+});
+
+export { tryLogin, getPresence, submitPresence, getAula };
